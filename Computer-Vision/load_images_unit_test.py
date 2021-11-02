80c55448acec377638ddb822df9ad8a06e96d30e
Brennen Chadburn <43618554+bchadburn@users.noreply.github.com>
2021-11-02 00:33:50 -0600

    Add unit test

---
+++ Computer-Vision/load_images_unit_test.py
@@ -0,0 +1,33 @@
+import unittest
+from training import Dataset
+import os
+from utils.config import *
+
+batch_size = 32  # For tests below, exact value doesn't matter
+
+
+class TestLoadImages(unittest.TestCase):
+    def setUp(self):
+        self.Dataset = Dataset(TRAINING_IMAGES_PATH, batch_size, IMAGE_SIZE)
+        self.Dataset.classes = ALL_CLASSES
+        self.image_filenames, self.image_targets = self.Dataset._load_images()
+
+    def test_same_length(self):
+        self.assertEqual(len(self.image_filenames), len(self.image_targets))
+
+    def test_correct_format(self):
+        prefix_list = [str(i) + '-' for i in self.Dataset.classes]
+        for i in self.image_filenames:
+            self.assertIn(i.split(os.path.sep)[1][0:2], prefix_list)
+            self.assertTrue(i.endswith('.jpg'))
+
+    def test_image_targets(self):
+        classes = [str(i) for i in ALL_CLASSES]
+        for i in self.image_targets:
+            self.assertIn(i, classes)
+
+
+if __name__ == '__main__':
+    print("Unit test function _load_images from training.py script")
+    print("training images file path:", TRAINING_IMAGES_PATH)
+    unittest.main()
