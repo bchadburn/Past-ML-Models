da8878956ec5762112a8ba6955890d73fdbedd56
Brennen Chadburn <43618554+bchadburn@users.noreply.github.com>
2021-11-02 00:31:50 -0600

    Update single_image_prediction.py

--- Computer-Vision/single_image_prediction.py
+++ Computer-Vision/single_image_prediction.py
@@ -1,20 +1,7 @@
 from utils.process_image import *
-import os
 import argparse
-
-IMAGE_SIZE = 224
-
-CLASS_LABELS = ('outdoor', 'indoor')
-
-MODEL_PATH = "model_results/fine_tuned_model"
-IMAGE_FOLDER_PATH = "indoor_outdoor_images"
-
-
-def dir_file(path):
-    if os.path.isfile(path):
-        return path
-    else:
-        raise argparse.ArgumentTypeError(f"image_name:{path} is not a valid path")
+from utils.utils import dir_file
+from utils.config import *
 
 
 if __name__ == "__main__":
@@ -22,13 +9,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", dest="image_path", required=True,
                         help="Full path to image, must be .jpg", metavar="FILE", type=lambda x: dir_file(x))
-
     args, _ = parser.parse_known_args()
+
+    if not args.image_path.endswith(".jpg"):
+        if not args.image_path.endswith(".jpeg"):
+            raise TypeError("Invalid file to return model predictions, file needs to end with .jpg")
+
     image = process_single_image(args.image_path, IMAGE_SIZE)
-    fine_tuned_model = tf.keras.models.load_model(MODEL_PATH)
+    fine_tuned_model = tf.keras.models.load_model(os.path.join(MODEL_RESULTS_PATH, "fine_tuned_model"))
 
     prediction = fine_tuned_model.predict(image)
     pred_index = np.argmax(prediction, axis=1)
-    print('class: ', CLASS_LABELS[pred_index[0]])
-    print('score: ', prediction[0][pred_index][0])
-
+    prediction = round(prediction[0][pred_index][0], 4)
+    print('class: ', PRED_CLASS_NAMES[pred_index[0]])
+    print('score: ', f'{prediction:.2%}')
